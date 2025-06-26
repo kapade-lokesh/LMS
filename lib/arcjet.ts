c3c9710d603ed45db57cb733a53562247f6dd6b5
kapade-lokesh <lkkapade@gmail.com>
2025-06-26 08:19:19 +0530

    secure request with arcjet

---
+++ lib/arcjet.ts
@@ -0,0 +1,24 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  shield,
+  sensitiveInfo,
+  slidingWindow,
+} from "@arcjet/next";
+import { env } from "./env";
+
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  shield,
+  sensitiveInfo,
+  slidingWindow,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+  rules: [shield({ mode: "LIVE" })],
+});
