5b2a2fdcd8dbbe1f93e63a6bbdb202987147a0e0
kapade-lokesh <lkkapade@gmail.com>
2025-06-28 17:24:09 +0530

    create course form

--- app/admin/courses/create/page.tsx
+++ app/admin/courses/create/page.tsx
@@ -1,15 +1,66 @@
-import { buttonVariants } from "@/components/ui/button";
+"use client";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Card,
+  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { ArrowLeft } from "lucide-react";
+import {
+  courseCategories,
+  courseLevels,
+  courseSchema,
+  CourseSchemaType,
+  courseStatus,
+} from "@/lib/zodSchemas";
+import { ArrowLeft, SparkleIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import slugify from "slugify";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+  SelectItem,
+} from "@/components/ui/select";
 
 const CourserCreationPage = () => {
+  const form = useForm<CourseSchemaType>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      fileKey: "",
+      price: 0,
+      duration: 0,
+      level: "Beginner",
+      category: "Development",
+      status: "Draft",
+      slug: "",
+      smallDescription: "",
+    },
+  });
+
+  function onSubmit(values: CourseSchemaType) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <>
       <div className="flex items-center gap-4">
@@ -31,6 +82,230 @@ const CourserCreationPage = () => {
           <CardTitle>Basic Infromation</CardTitle>
           <CardDescription>Provide Basic Info</CardDescription>
         </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Title" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex gap-4 items-end">
+                <FormField
+                  control={form.control}
+                  name="slug"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Slug</FormLabel>
+                      <FormControl>
+                        <Input placeholder="Slug" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <Button
+                  type="button"
+                  onClick={() => {
+                    const titleValue = form.getValues("title");
+                    const slug = slugify(titleValue);
+                    form.setValue("slug", slug, { shouldValidate: true });
+                  }}
+                >
+                  Generate Slug <SparkleIcon className="size-4 ml-1" />
+                </Button>
+              </div>
+
+              <FormField
+                control={form.control}
+                name="smallDescription"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Small Description</FormLabel>
+                    <FormControl>
+                      <Textarea
+                        className="min-h-[120px]"
+                        placeholder="Small Description"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Description" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="fileKey"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Thumbnail Image</FormLabel>
+                    <FormControl>
+                      <Input placeholder="thumbnail url" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <FormField
+                  control={form.control}
+                  name="category"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Category</FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select Category" />
+                              <FormControl>
+                                <SelectContent>
+                                  {courseCategories.map((category) => (
+                                    <SelectItem key={category} value={category}>
+                                      {category}
+                                    </SelectItem>
+                                  ))}
+                                </SelectContent>
+                              </FormControl>
+                            </SelectTrigger>
+                          </FormControl>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="level"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Levels</FormLabel>
+                      <FormControl>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
+                          <FormControl>
+                            <SelectTrigger className="w-full">
+                              <SelectValue placeholder="Select Level" />
+                              <FormControl>
+                                <SelectContent>
+                                  {courseLevels.map((level) => (
+                                    <SelectItem key={level} value={level}>
+                                      {level}
+                                    </SelectItem>
+                                  ))}
+                                </SelectContent>
+                              </FormControl>
+                            </SelectTrigger>
+                          </FormControl>
+                        </Select>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="duration"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Duration</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="number"
+                          placeholder="Duration"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="duration"
+                  render={({ field }) => (
+                    <FormItem className="w-full">
+                      <FormLabel>Price (&#8377;)</FormLabel>
+                      <FormControl>
+                        <Input type="number" placeholder="Price" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <FormField
+                control={form.control}
+                name="status"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Status</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Select Status" />
+                            <FormControl>
+                              <SelectContent>
+                                {courseStatus.map((status) => (
+                                  <SelectItem key={status} value={status}>
+                                    {status}
+                                  </SelectItem>
+                                ))}
+                              </SelectContent>
+                            </FormControl>
+                          </SelectTrigger>
+                        </FormControl>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button>Create Course +</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </>
   );
--- lib/zodSchemas.ts
+++ lib/zodSchemas.ts
@@ -2,6 +2,19 @@ import { z } from "zod";
 
 export const courseLevels = ["Beginner", "Intermediate", "Advanced"] as const;
 export const courseStatus = ["Draft", "Published", "Archived"] as const;
+export const courseCategories = [
+  "Development",
+  "Business",
+  "Finance",
+  "IT & Software",
+  "Office Productivity",
+  "Personal Development",
+  "Design",
+  "Marketing",
+  "Health & Fitness",
+  "Music",
+  "Teaching & Academics",
+] as const;
 
 export const courseSchema = z.object({
   title: z
@@ -20,7 +33,7 @@ export const courseSchema = z.object({
     .min(1, { message: "Duration must be at least 1 hour long" })
     .max(500, { message: "Duration must be at most 500 hours long" }),
   level: z.enum(courseLevels, { message: "Level is required" }),
-  category: z.string(),
+  category: z.enum(courseCategories),
   smallDescription: z
     .string()
     .min(3, { message: "smallDescription must be at least 3 char long" })
@@ -28,3 +41,5 @@ export const courseSchema = z.object({
   slug: z.string().min(3, { message: "Slug must be at least 3 char long" }),
   status: z.enum(courseStatus, { message: "Status is required" }),
 });
+
+export type CourseSchemaType = z.infer<typeof courseSchema>;
